26e66f8af5d7eae229d1c4a5d6349107acccd950
bmarkons <mamaveb@gmail.com>
2017-12-17 13:01:01 +0100

    Add script which uses script - for testing

---
+++ scripts/script.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require "script"
+
+script = Script.new
+
+script.step("step 1") do
+  puts "Step 1"
+  puts "Completed"
+end
+
+script.step("step 2") do
+  puts "Step 2"
+  puts "Completed"
+end
+
+script.run
